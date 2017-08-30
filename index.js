1d77eeee93b5dae055901465049bc17a23b01ea3
Justyna Field <justyna.field@gmail.com>
2017-08-30 13:39:29 -0600

    add accorion tabs

---
+++ index.js
@@ -0,0 +1,25 @@
+// document.ready(() => {
+//   loadFirstTab()
+// })
+
+
+$('.tab1').on('click', () => {
+  $('.container').addClass('hidden')
+  $('#tab-container1').removeClass('hidden')
+})
+
+
+$('.tab2').on('click', () => {
+  $('.container').addClass('hidden')
+  $('#tab-container2').removeClass('hidden')
+})
+
+$('.tab3').on('click', () => {
+  $('.container').addClass('hidden')
+  $('#tab-container3').removeClass('hidden')
+})
+
+$('.tab4').on('click', () => {
+  $('.container').addClass('hidden')
+  $('#tab-container4').removeClass('hidden')
+})
